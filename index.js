e68b2e1ca67271a4f9cab96cbc64aeb029009b00
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2021-10-03 12:02:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-	if ('geolocation' in navigator) {
-		console.log('geolocation available');
-		navigator.geolocation.getCurrentPosition(position => {
-			lat = position.coords.latitude;
-			lon = position.coords.longitude;
-			console.log(lat, lon);
-			document.getElementById('lat').textContent = lat;
-			document.getElementById('lon').textContent = lon;
-
-			const mymap = L.map('mymap').setView([lat, lon], 15);
-			const attribution =
-			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-			const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-			const tiles = L.tileLayer(tileUrl, { attribution });
-			tiles.addTo(mymap);
-			const marker = L.marker([lat, lon]).addTo(mymap);
-		});
-	} else {
-		console.log('geolocation not available');
-	}
+document.addEventListener("DOMContentLoaded", function () {
+    if ("geolocation" in navigator) {
+        console.log("geolocation available");
+        navigator.geolocation.getCurrentPosition((position) => {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            console.log(lat, lon);
+            document.getElementById("lat").textContent = lat;
+            document.getElementById("lon").textContent = lon;
+            const mymap = L.map("mymap").setView([lat, lon], 15);
+            const attribution =
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+            const tileUrl =
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+            const tiles = L.tileLayer(tileUrl, { attribution });
+            tiles.addTo(mymap);
+            L.marker([lat, lon]).addTo(mymap);
+        });
+    } else {
+        console.log("geolocation not available");
+    }
 });
