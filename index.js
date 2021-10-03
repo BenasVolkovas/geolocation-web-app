e4fbe3b448ca289f4ea35b5aa126f6ffe39a2213
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2021-10-03 11:25:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-window.onload = function() {
-	let startPos;
-
-	let geoOptions = {
-		timeout: 10 * 1000,
-		enableHighAccuracy: true
-	};
-
-	let geoSuccess = function(position) {
-		startPos = position;
-		document.getElementById('lat').innerHTML = startPos.coords.latitude;
-		document.getElementById('lon').innerHTML = startPos.coords.longitude;
-		document.getElementById('acc').innerHTML = startPos.coords.accuracy;
-	};
-
-	let geoError = function(error) {
-		alert('Error occurred. Error code: ' + error.code);
-		// error.code can be:
-		//   0: unknown error
-		//   1: permission denied
-		//   2: position unavailable (error response from location provider)
-		//   3: timed out
-	};
-
-	navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
-
 document.addEventListener("DOMContentLoaded", function() {
+	navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+	if ('geolocation' in navigator) {
+		console.log('geolocation available');
+		navigator.geolocation.getCurrentPosition(position => {
+			lat = position.coords.latitude;
+			lon = position.coords.longitude;
+			console.log(lat, lon);
+			document.getElementById('lat').textContent = lat;
+			document.getElementById('long').textContent = lon;
 
+			const mymap = L.map('mymap').setView([lat, lon], 15);
+			const attribution =
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+			const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+			const tiles = L.tileLayer(tileUrl, { attribution });
+			tiles.addTo(mymap);
+			const marker = L.marker([lat, lon]).addTo(mymap);
+		});
+	} else {
+		console.log('geolocation not available');
+	}
 });
