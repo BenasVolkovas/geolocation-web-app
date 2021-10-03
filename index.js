6ce259b56227cee19fd0c1152e8355b61040f3c4
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2021-10-03 12:09:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const options = {
+        enableHighAccuracy: true,
+    };
+
+    const success = (position) => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        console.log(lat, lon);
+        document.getElementById("lat").textContent = lat;
+        document.getElementById("lon").textContent = lon;
+        const mymap = L.map("mymap").setView([lat, lon], 15);
+        const attribution =
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+        const tileUrl =
+            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+        const tiles = L.tileLayer(tileUrl, { attribution });
+        tiles.addTo(mymap);
+        L.marker([lat, lon]).addTo(mymap);
+    };
+
+    const error = (error) => {
+        document.getElementById("error").textContent = error;
+        console.log(error);
+    };
+    
+    
     if ("geolocation" in navigator) {
         console.log("geolocation available");
-        navigator.geolocation.getCurrentPosition((position) => {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            console.log(lat, lon);
-            document.getElementById("lat").textContent = lat;
-            document.getElementById("lon").textContent = lon;
-            const mymap = L.map("mymap").setView([lat, lon], 15);
-            const attribution =
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-            const tileUrl =
-                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-            const tiles = L.tileLayer(tileUrl, { attribution });
-            tiles.addTo(mymap);
-            L.marker([lat, lon]).addTo(mymap);
-        }, (error) => {
-            document.getElementById("error").textContent = error;
-            console.log(error);
-        };
+        navigator.geolocation.getCurrentPosition(success, error, options);
     } else {
         document.getElementById("error").textContent = "geolocation not available";
         console.log("geolocation not available");
